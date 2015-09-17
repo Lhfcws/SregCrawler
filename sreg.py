ecf7af5fd1aa8720e7f156bae25e5307ec2f4051
lhfcws <lhfcws@163.com>
2015-09-17 11:09:28 +0800

    commit

--- sreg.py
+++ sreg.py
@@ -34,19 +34,19 @@ def check(plugin, passport, passport_type):
     judge_yes_keyword = plugin['status']['judge_yes_keyword']
     judge_no_keyword = plugin['status']['judge_no_keyword']
     headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
-                'Host': urlparse.urlparse(url).netloc,
-                'Referer': url,
-                }
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
+        'Host': urlparse.urlparse(url).netloc,
+        'Referer': url,
+    }
     if plugin['request']['method'] == "GET":
         try:
             content = requests.get(url, headers=headers, timeout=8).content
-            content = unicode(content, "utf-8")
-        except Exception, e:
-            print inRed('\n[-] %s ::: %s\n' % (app_name, str(e)))
+            content = sys.unicode(content, "utf-8")
+        except Exception as e:
+            print(inRed('\n[-] %s ::: %s\n' % (app_name, str(e))))
             return
         if judge_yes_keyword in content and judge_no_keyword not in content:
-            print u"[{0}] {1}".format(category, ('%s (%s)' % (app_name, website)))
+            print(u"[{0}] {1}".format(category, ('%s (%s)' % (app_name, website))))
             icon = plugin['information']['icon']
             desc = plugin['information']['desc']
             output_add(category, app_name, website, passport, passport_type, icon, desc)
@@ -55,7 +55,8 @@ def check(plugin, passport, passport_type):
     elif plugin['request']['method'] == "POST":
         post_data = plugin['request']['post_fields']
         if post_data.values().count("") != 1:
-            print "The POST field can only leave a null value."
+            print
+            "The POST field can only leave a null value."
             return
         for k, v in post_data.iteritems():
             if v == "":
@@ -65,23 +66,23 @@ def check(plugin, passport, passport_type):
             encoding = chardet.detect(content)["encoding"]
             if encoding == None:
                 encoding = "utf-8"
-            content = unicode(content, encoding)
-        except Exception, e:
-            print e, app_name
+            content = sys.unicode(content, encoding)
+        except Exception as e:
+            print(str(e) + str(app_name))
             return
         if judge_yes_keyword in content and judge_no_keyword not in content:
-            print u"[{0}] {1}".format(category, ('%s (%s)' % (app_name, website)))
+            print(u"[{0}] {1}".format(category, ('%s (%s)' % (app_name, website))))
             icon = plugin['information']['icon']
             desc = plugin['information']['desc']
             output_add(category, app_name, website, passport, passport_type, icon, desc)
         else:
             pass
     else:
-        print u"{}:::Error!".format(plugin['request']['name'])
+        print(u"{}:::Error!".format(plugin['request']['name']))
 
 
 def main():
-    reload(sys)
+    sys.reload(sys)
     sys.setdefaultencoding("utf-8")
     parser = argparse.ArgumentParser(description="Check how many Platforms the User registered.")
     parser.add_argument("-u", action="store", dest="user")
@@ -103,24 +104,32 @@ def main():
     '''
     all_argument = [parser_argument.cellphone, parser_argument.user, parser_argument.email]
     plugins = glob.glob("./plugins/*.json")
-    print inGreen(banner)
-    print '[*] App: Search Registration'
-    print '[*] Version: V1.0(20150303)'
-    print '[*] Website: buzz.beebeeto.com'
+    print
+    inGreen(banner)
+    print
+    '[*] App: Search Registration'
+    print
+    '[*] Version: V1.0(20150303)'
+    print
+    '[*] Website: buzz.beebeeto.com'
     file_name = ""
     if all_argument.count(None) != 2:
-        print '\nInput "-h" view the help information.'
+        print
+        '\nInput "-h" view the help information.'
         sys.exit(0)
     if parser_argument.cellphone:
-        print inRed('\n[+] Phone Checking: %s\n') % parser_argument.cellphone
+        print
+        inRed('\n[+] Phone Checking: %s\n') % parser_argument.cellphone
         file_name = "cellphone_" + str(parser_argument.cellphone)
         output_init(file_name, "Phone: ", str(parser_argument.cellphone))
     if parser_argument.user:
-        print inRed('\n[+] Username Checking: %s\n') % parser_argument.user
+        print
+        inRed('\n[+] Username Checking: %s\n') % parser_argument.user
         file_name = "user_" + str(parser_argument.user)
         output_init(file_name, "UserName: ", str(parser_argument.user))
     if parser_argument.email:
-        print inRed('\n[+] Email Checking: %s\n') % parser_argument.email
+        print
+        inRed('\n[+] Email Checking: %s\n') % parser_argument.email
         file_name = "email_" + str(parser_argument.email)
         output_init(file_name, "E-mail: ", str(parser_argument.email))
     jobs = []
@@ -128,18 +137,21 @@ def main():
         with open(plugin) as f:
             try:
                 content = json.load(f)
-            except Exception, e:
-                print e, plugin
+            except Exception as e:
+                print(str(e) + str(plugin))
                 continue
         if parser_argument.cellphone:
-            p = multiprocessing.Process(target=check, args=(content,unicode(parser_argument.cellphone, "utf-8"), "cellphone"))
+            p = multiprocessing.Process(target=check,
+                                        args=(content, sys.unicode(parser_argument.cellphone, "utf-8"), "cellphone"))
         elif parser_argument.user:
-            p = multiprocessing.Process(target=check, args=(content,unicode(parser_argument.user, "utf-8"), "user"))
+            p = multiprocessing.Process(target=check,
+                                        args=(content, sys.unicode(parser_argument.user, "utf-8"), "user"))
         elif parser_argument.email:
-            p = multiprocessing.Process(target=check, args=(content,unicode(parser_argument.email, "utf-8"), "email"))
+            p = multiprocessing.Process(target=check,
+                                        args=(content, sys.unicode(parser_argument.email, "utf-8"), "email"))
         p.start()
         jobs.append(p)
-    while(sum([i.is_alive() for i in jobs]) != 0):
+    while (sum([i.is_alive() for i in jobs]) != 0):
         pass
     for i in jobs:
         i.join()
---
+++ youzan.py
@@ -0,0 +1,49 @@
+import sys, os, time
+
+def get_all_phones(fl):
+    # return ["15800010588"]
+    with open(fl, "r") as fp:
+    # with open("youzan/data/all_numbers.txt", "r") as fp:
+        ret = []
+        for line in fp.readlines():
+            ret.append(line.strip())
+        return ret
+
+def run(phones, dr):
+    progress = 0
+    path = "youzan/result/" + dr
+    os.system("mkdir -p " + path)
+
+    cmd = "python sreg.py -c %s > " + path + "/%s.txt"
+    for phone in phones:
+        os.system(cmd % (phone, phone))
+        progress += 1
+        print(phone + " done. " + str(progress))
+
+def main():
+    root = "youzan/data/split100/"
+    os.system("mkdir -p youzan/result")
+    files = os.listdir(root)
+    progress = 0
+
+    res_files = os.listdir("youzan/result")
+
+    for fl in files:
+        if res_files.count(fl) > 0:
+            print("[MAIN] Partition %s has been done, skip it." % fl)
+            continue
+        path = root + fl
+        phones = get_all_phones(path)
+        print("[MAIN] Get phones size: " + str(len(phones)) )
+        run(phones, fl)
+        progress += 100
+        print("[MAIN] Run " + fl + " done. Progress: " + str(progress))
+
+        os.system("rm reports/cellphone_*.html")
+
+        # Sleep 2 seconds
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
