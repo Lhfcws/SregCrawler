ef098f149f20e535efabc799586b64c7ad9a8edf
lhfcws <lhfcws@163.com>
2015-09-18 19:37:46 +0800

    add youzan

---
+++ youzan/split.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# coding: utf-8
+__author__ = 'lhfcws'
+
+import socket, os
+
+os.system("mkdir -p data/split")
+# TOTAL
+TOTAL = 50000
+SPLIT = 6
+
+block = TOTAL / SPLIT + 1
+host = socket.gethostname()
+# host = "dev6"
+index = int(list(host)[-1])
+start = block * (index - 1)
+end = start + block
+if end > TOTAL:
+    end = TOTAL
+
+diff = end - start
+CMD = "head -%d data/all_numbers.txt | tail -%d  > data/split.txt" % (end, diff)
+
+print(CMD)
+
+## Split file
+CMD = "cp split.txt data/ ; cd data ; split split.txt -l 100 ; rm split.txt"
+os.system(CMD)
